43c56e056f5279a367f46c84aca50f36cbc34c52
nevext <215304957+nevext@users.noreply.github.com>
2026-04-28 00:38:56 -0400

    working

--- models.py
+++ models.py
@@ -1,2 +1,8 @@
-class Category:       #(david neves) tinha esquecido da hierarquia do que criar primeiro classe que não depende de ninguém logo
-     
+                    #(david neves) tinha esquecido da hierarquia do que criar primeiro classe que não depende de ninguém logo, lembrando q uma class é a definicação ou o
+                        # modelo de como algo vai ser e nao um objeto tlgd
+class Category:
+    def __init__(self, id: int, name:str):
+        self.__id = id
+        self.__name = name
+
+        #
